6606f1cbf8bea90263a2392a83bfce35b40abb12
gfucci <gfucci_2005@hotmail.com>
2022-07-05 17:16:14 -0300

    Conecta ao mongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,30 @@
+require("dotenv").config()
+
 const express = require("express")
 const path = require("path")
 const cors = require("cors")
 
-const port = 5000
+const port = process.env.PORT
 
 const app = express()
 
-//config JSON
+//solve CORS
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }))
+
+//upload directory
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")))
+
+//DB connection
+require("./config/db.js")
+
+//config JSON and form data response
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+// routes
+const router = require("./routes/Router.js")
+
+app.use(router)
 
 app.listen(port, () => {
     console.log(`App rodando na porta ${port}`)
