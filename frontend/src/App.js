23cadb327892c461dc7532064b7fb60f41c57c06
Gabriel Fucci <gfucci_2005@hotmail.com>
2022-07-12 22:51:44 -0300

    Cria navbar, footer e rotas iniciais

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,34 @@
+//CSS
 import './App.css';
+
+//Router
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+
+//components
+import NavBar from './components/Navbar';
+import Footer from './components/Footer';
+
+//pages
+import Home from './pages/Home/Home';
+import Register from './pages/Auth/Register';
+import Login from './pages/Auth/Login';
+
+
 
 function App() {
   return (
     <div className="App">
-      <h1>ReactGram</h1>
+      <BrowserRouter>
+        <NavBar />
+        <div className="container">
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/register' element={<Register />} />
+            <Route path='/login' element={<Login />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
