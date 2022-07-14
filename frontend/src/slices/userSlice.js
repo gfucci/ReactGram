c71737e37bbc03b264a039ebc6c2f2ce05a41372
gfucci <gfucci_2005@hotmail.com>
2022-07-14 18:22:29 -0300

    Cria slice e service dos usuarios

---
+++ frontend/src/slices/userSlice.js
@@ -0,0 +1,50 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import userService from "../services/userService";
+
+const initialState = {
+    user: {},
+    loading: false,
+    error: false,
+    success: false,
+    message: null
+}
+
+//get user details
+export const profile = createAsyncThunk(
+    "user/profile",
+    async (user, thunkAPI) => {
+        const token = thunkAPI.getState().auth.user.token
+
+        const data = await userService(user, token)
+
+        return data
+    }
+)
+
+// create slice
+export const userSlice = createSlice({
+    name: "user",
+    initialState,
+    reducers: {
+        resetMessage: (state) => {
+            state.message = null
+        },
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(register.pending, (state) => {
+                state.loading = true
+                state.error = false
+            })
+            .addCase(register.fulfilled, (state, action) => {
+                state.loading = false
+                state.error = null
+                state.success = true
+                state.user = action.payload
+            })
+    }  
+})
+
+export const { resetMessage } = userSlice.actions
+export default userSlice.reducer
