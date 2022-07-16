697f935cf6ae5c72464e5801a4fadb47853dd162
Gabriel Fucci <gfucci_2005@hotmail.com>
2022-07-16 18:42:28 -0300

    Cria service e slice das fotos

---
+++ frontend/src/services/photoService.js
@@ -0,0 +1,8 @@
+import { api } from "../utils/config";
+import { requestConfig } from "../utils/config";
+
+const photoService = {
+
+}
+
+export default photoService
---
+++ frontend/src/slices/photoSlice.js
@@ -0,0 +1,25 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
+import photoService from "../services/photoService";
+
+const initialState ={
+    photos: [],
+    photo: {},
+    error: false,
+    loading: false,
+    success: false,
+    message: null
+}
+
+export const photoSlice = createSlice({
+    name: "photo",
+    initialState,
+    reducers: {
+        resetMessage: (state) => {
+            state.message = null
+        }
+    }
+})
+
+export const { resetMessage } = photoSlice.actions
+export default photoSlice.reducer
